66bf01b0f21723cbc39cf941f4bf1026b77f725b
Robert Johnson <johnson.robert.333@gmail.com>
2026-02-26 12:59:42 -0600

    add Dec 2025 curve

--- Data-Scripts/data_S-std-curves.R
+++ Data-Scripts/data_S-std-curves.R
@@ -48,6 +48,47 @@ summary(lm(conc_uM ~ abs_corr, data = std_apr25))  # R2 = 0.995
 
 
 
+#--
+# Low curve, Dec. 2025
+#--
+
+# Standard curve using MQ water instead of ZnAc for all standards, blanks, and dilutions
+
+   # S std date: 6/20/2023
+   # Diamine reag. (L) date: 4/05/2025
+   # Run by: RA Johnson & MG Teter
+
+
+# raw data from spec run
+std_curve_dec25 <- read_csv("Data/Spec Data/Sulfide_Low_standard_curve_Dec_2025.csv") %>%
+   janitor::remove_empty(which = 'rows')
+
+
+# Clean up the data
+std_dec25 <- std_curve_dec25 %>%
+   # remove 'zero'
+   filter(!(vial == "Zero")) %>%
+   # remove checks
+   filter(!(str_detect(vial, pattern='chk')))
+
+
+# Calculate corrected absorbance for standards
+std_dec25 <- std_dec25 %>%
+   filter(!(vial == "Blank")) %>%
+   # correct for blank absorbance
+   mutate(abs_blk_corr = abs_667 - (std_dec25 %>% filter(vial == "Blank") %>% pull(abs_667) %>% mean)) %>%
+   # correct for dilution factor
+   mutate(abs_corr = abs_blk_corr * dilution)
+
+
+# View the curve
+windows(); ggplot(std_dec25, aes(x = abs_corr, y = conc_uM)) + geom_point()
+graphics.off()
+summary(lm(conc_uM ~ abs_corr, data = std_dec25))  # R2 = 0.9965
+
+
+
+
 #~~~
 # Equation for concentration - absorbance relationship
 #~~~
